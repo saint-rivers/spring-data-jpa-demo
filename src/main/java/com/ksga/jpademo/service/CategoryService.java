60ae1c445966eb7d7ec1e296748c3f31efc3f711
eamdayan <eam.dayan@gmail.com>
2022-08-30 17:47:38 +0700

    reset code for demo

--- src/main/java/com/ksga/jpademo/service/CategoryService.java
+++ src/main/java/com/ksga/jpademo/service/CategoryService.java
@@ -1,10 +1,7 @@
 package com.ksga.jpademo.service;
 
-import com.ksga.jpademo.model.entities.Category;
-import org.springframework.data.domain.Page;
 
 public interface CategoryService {
 
-    Page<Category> findAll(Integer page, Integer size);
 }
 
--- src/main/java/com/ksga/jpademo/service/CategoryServiceImpl.java
+++ src/main/java/com/ksga/jpademo/service/CategoryServiceImpl.java
@@ -1,10 +1,7 @@
 package com.ksga.jpademo.service;
 
-import com.ksga.jpademo.model.entities.Category;
 import com.ksga.jpademo.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +10,4 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    @Override
-    public Page<Category> findAll(Integer page, Integer size) {
-        return categoryRepository.findAll(PageRequest.of(page, size));
-    }
 }
