68ea659e41680f7bb308c8e98bb0204d281513af
eamdayan <eam.dayan@gmail.com>
2022-08-31 09:38:26 +0700

    reset code for demo

--- src/main/java/com/ksga/jpademo/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ksga.jpademo.repository;
-
-import com.ksga.jpademo.model.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/com/ksga/jpademo/service/CategoryServiceImpl.java
+++ src/main/java/com/ksga/jpademo/service/CategoryServiceImpl.java
@@ -1,13 +1,10 @@
 package com.ksga.jpademo.service;
 
-import com.ksga.jpademo.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
-
-    private final CategoryRepository categoryRepository;
 
 }
